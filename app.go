1ce3ae45a7574df00287f700d40c21699b7cf9f3
Abhinav-26 <abhinavdubey616@gmail.com>
2021-06-29 00:32:15 +0530

    Dockerfile added

--- app.go
+++ app.go
@@ -18,8 +18,8 @@ func main() {
 
 	http.HandleFunc("/", serveTemplate)
 
-	log.Println("Listening on :3000...")
-	err := http.ListenAndServe(":3000", nil)
+	log.Println("Listening on :8080...")
+	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Fatal(err)
 	}
